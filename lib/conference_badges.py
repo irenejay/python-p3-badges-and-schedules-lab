b9d43518bf58b7a971b901aa6d265a0198764e2b
irene <irenejim9504@gmail.com>
2024-03-04 00:38:17 +0300

    added functionality

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,39 @@
 def badge_maker(name):
-    return None
+    return "Hello, my name is " + name + "."
+
+
+print(badge_maker("Arel"))
+
 
 def batch_badge_creator(names):
-    return None
+    badge_messages = []
+    for name in names:
+        badge_messages.append("Hello, my name is " + name + ".")
+    return badge_messages
 
-def assign_rooms(names):
-    return None
+def assign_rooms(speakers):
+    room_assignments = []
+    for i, speaker in enumerate(speakers, start=1):
+        room_assignments.append("Hello, {}! You'll be assigned to room {}!".format(speaker, i))
+    return room_assignments
+
+def batch_badge_creator(names):
+    badge_messages = []
+    for name in names:
+        badge_messages.append("Hello, my name is " + name + ".")
+    return badge_messages
+
+def assign_rooms(speakers):
+    room_assignments = []
+    for i, speaker in enumerate(speakers, start=1):
+        room_assignments.append("Hello, {}! You'll be assigned to room {}!".format(speaker, i))
+    return room_assignments
 
 def printer(names):
-    return None
+    badges = batch_badge_creator(names)
+    for badge in badges:
+        print(badge)
+    
+    assignments = assign_rooms(names)
+    for assignment in assignments:
+        print(assignment)
